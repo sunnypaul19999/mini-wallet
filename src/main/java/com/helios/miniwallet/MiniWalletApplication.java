9e66e368e4ff6f1be7fb0cba7e417b85609105c9
sunnypaul19999 <sunnypaul19999@gmail.com>
2022-09-19 15:25:26 +0530

    project init

---
+++ src/main/java/com/helios/miniwallet/MiniWalletApplication.java
@@ -0,0 +1,13 @@
+package com.helios.miniwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helios/miniwallet/MiniWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helios.miniwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
